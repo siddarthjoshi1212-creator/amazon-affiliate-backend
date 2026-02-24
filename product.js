1468bb86864120c0eb1cd382968690fc200b3593
siddarthjoshi1212-creator <siddarthjoshi1212@gmail.com>
2026-02-24 16:10:25 +0530

    add product model correctly

---
+++ product.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  image: String,
+  price: String,
+  affiliateLink: String,
+  category: String,
+  isFeatured: Boolean,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
